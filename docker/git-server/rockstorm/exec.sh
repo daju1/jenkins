05a9efda40858f5eafb026d134033be60e546965
Alexey Drozdov <daju@yandex.ru>
2025-06-26 14:07:57 +0000

    update README.md

--- docker/git-server/rockstorm/exec.sh
+++ docker/git-server/rockstorm/exec.sh
@@ -1,4 +1,4 @@
-docker exec -it rockstorm_git_server ifconfig
-docker exec -it rockstorm_git_server git --version
-docker exec -it rockstorm_git_server ps aux
-docker exec -it rockstorm_git_server /bin/sh
+docker exec -it git_server_rockstorm ifconfig
+docker exec -it git_server_rockstorm git --version
+docker exec -it git_server_rockstorm ps aux
+docker exec -it git_server_rockstorm /bin/sh
